d255785b8a7ce5b263be3e3df6bdc7a7e012ff66
Vincent <vincentstettler@gmail.com>
2020-03-21 14:32:18 +0100

    feat(vote): add parser for individual vote datasets

--- vote_parsers.py
+++ vote_parsers.py
@@ -2,6 +2,43 @@ import numpy as np
 import pandas as pd
 
 from utils import pd_convert_to_int
+from vote_common_strings import (KANTON_NAME, KANTON_NR, STIMMBERECHTIGTE,
+                                 ABGEGEBENE_STIMMEN, LEER, UNGUELTIGE_STIMMEN, GUELTIGE_STIMMEN,
+                                 JA_IN_PERC, JA_STIMMEN, NEIN_STIMMEN, STIMMBETEILIGUNG)
+
+
+def parse_abstimmung_nach_kantonen(path: str, skip_start=9, skip_end=6):
+    """
+    Tables: Every BFS table about one 'Volksabstimmung, .. nach Kantonen'.
+    Note: This method will be iteratively made more general.
+          Currently, it will probably break for most tables.
+    """
+    column_names = [KANTON_NR, KANTON_NAME, STIMMBERECHTIGTE, ABGEGEBENE_STIMMEN,
+                    STIMMBETEILIGUNG, LEER, UNGUELTIGE_STIMMEN, GUELTIGE_STIMMEN, JA_STIMMEN, NEIN_STIMMEN,
+                    JA_IN_PERC]
+    column_dtypes = {
+        column_names[1]: object,
+        column_names[4]: np.float64,
+        column_names[10]: np.float64
+    }
+    converters = {
+        column_names[0]: pd_convert_to_int,
+        column_names[2]: pd_convert_to_int,
+        column_names[3]: pd_convert_to_int,
+        column_names[5]: pd_convert_to_int,
+        column_names[6]: pd_convert_to_int,
+        column_names[7]: pd_convert_to_int,
+        column_names[8]: pd_convert_to_int,
+        column_names[9]: pd_convert_to_int
+    }
+    df = pd.read_excel(path,
+                       skiprows=skip_start,
+                       skipfooter=skip_end,
+                       names=column_names,
+                       converters=converters,
+                       dtype=column_dtypes)
+    df = df[df[KANTON_NR] != -1].reset_index(drop=True)
+    return df
 
 
 def parse_eidgenoessische_volksabstimmungen(path: str):
@@ -14,9 +51,9 @@ def parse_eidgenoessische_volksabstimmungen(path: str):
                    '1980-1989', '1970-1979', '1950-1969', '1900-1949', '1848-1899']
     header_skip_rows = [2, 2, 6, 6, 6, 6, 6, 6, 6]
     footer_skip_rows = [16, 24, 15, 14, 14, 14, 14, 14, 15]
-    column_names = ['Nr. der Vorlage', 'Datum', 'Gegenstand', 'Art', 'Stimmberechtigte',
-                    'Abgegebene Stimmen', 'Stimmbeteiligung in %', 'Leer', 'Ungültig',
-                    'Gültige Stimmen', 'JA-Stimmen', 'NEIN-Stimmen', 'JA in %',
+    column_names = ['Nr. der Vorlage', 'Datum', 'Gegenstand', 'Art', STIMMBERECHTIGTE,
+                    ABGEGEBENE_STIMMEN, STIMMBETEILIGUNG, LEER, UNGUELTIGE_STIMMEN,
+                    GUELTIGE_STIMMEN, JA_STIMMEN, NEIN_STIMMEN, JA_IN_PERC,
                     'Angenommen (A) Verworfen (V)', 'Annehmende Stände', 'Ablehnende Stände']
     column_dtypes = {
         column_names[0]: object,
